92bf1858153d6e8e8a6e547d447738c1fd721301
Huy Le <dacrazyazn@gmail.com>
2024-01-28 06:55:11 -0700

    cleaned up and ready for testing

--- lua/ogpt/module.lua
+++ lua/ogpt/module.lua
@@ -2,15 +2,11 @@
 local M = {}
 
 local Chat = require("ogpt.flows.chat")
-local Edits = require("ogpt.edits")
 local Actions = require("ogpt.flows.actions")
-local CodeCompletions = require("ogpt.flows.code_completions")
 
 M.open_chat = Chat.open
 M.focus_chat = Chat.focus
 M.open_chat_with_awesome_prompt = Chat.open_with_awesome_prompt
-M.edit_with_instructions = Edits.edit_with_instructions
 M.run_action = Actions.run_action
-M.complete_code = CodeCompletions.complete
 
 return M
