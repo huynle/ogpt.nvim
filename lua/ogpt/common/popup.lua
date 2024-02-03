7c0da2fe7eec4bbc68bedc246b2043c9aa9ea6c1
Huy Le <dacrazyazn@gmail.com>
2024-02-03 15:06:57 -0700

    forcing syntax on windows

--- lua/ogpt/common/popup.lua
+++ lua/ogpt/common/popup.lua
@@ -12,4 +12,13 @@ function Popup:init(options, edgy)
   Popup.super.init(self, options)
 end
 
+function Popup:mount()
+  Popup.super.mount(self)
+  -- syntax and filetype don't always get set in the correct order.
+  -- This for syntax to be the last thing to be set.
+  if self._.buf_options["syntax"] then
+    vim.api.nvim_buf_set_option(self.bufnr, "syntax", self._.buf_options["syntax"])
+  end
+end
+
 return Popup
