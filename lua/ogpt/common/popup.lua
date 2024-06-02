dbdbd7e534f3ef06c2909aa3dab6385faa925c44
Adrian Priestley <apriestley@gmail.com>
2024-06-02 19:18:19 -0230

    Experiment with syntax/filetype

--- lua/ogpt/common/popup.lua
+++ lua/ogpt/common/popup.lua
@@ -19,8 +19,11 @@ function Popup:mount()
   Popup.super.mount(self)
   -- syntax and filetype don't always get set in the correct order.
   -- This for syntax to be the last thing to be set.
+  if self._.buf_options["filetype"] then
+    vim.api.nvim_set_option_value("filetype", self._.buf_options["filetype"], { buf = self.bufnr })
+  end
   if self._.buf_options["syntax"] then
-    vim.api.nvim_buf_set_option(self.bufnr, "syntax", self._.buf_options["syntax"])
+    vim.api.nvim_set_option_value("syntax", self._.buf_options["syntax"], { buf = self.bufnr })
   end
 end
 
